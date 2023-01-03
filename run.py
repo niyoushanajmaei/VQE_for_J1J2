08cf5f6ba7154336032a8e02fa6c3ca23e600918
Praveen <pravissw@gmail.com>
2023-01-03 18:46:30 +0100

    run add_layers_fresh, runs as expected

--- run.py
+++ run.py
@@ -76,7 +76,7 @@ def testDynamicRunner():
     seed = 50
     # ansatz in {"TwoLocal", "FeulnerHartmann"}
     ansatz = "FeulnerHartmann"
-    layers = 5
+    layers = 7
     # optimizer in {"SLSQP", "SPSA", "ADAM", "COBYLA"}
     optimizer = "SLSQP"
 
@@ -85,9 +85,9 @@ def testDynamicRunner():
     J1 = 1
     J2 = 0.5
 
-    vqe_runner = DynamicVQERunner(m, n, J1, J2, h=0, ansatz_rep=layers, periodic_hamiltonian=False, ansatz=ansatz, optimizer=optimizer, totalMaxIter=100)
-    result = vqe_runner.run_dynamic_vqe(step_iter=10 ,large_gradient_add=True)
-
+    vqe_runner = DynamicVQERunner(m, n, J1, J2, h=0, ansatz_rep=layers, periodic_hamiltonian=False, ansatz=ansatz, optimizer=optimizer, totalMaxIter=1000)
+    # result = vqe_runner.run_dynamic_vqe(step_iter=10 ,large_gradient_add=True)
+    result = vqe_runner.run_dynamic_vqe(add_layers_fresh=True)
     print(result)
 
     print(f"The algorithm took {time.time()-start:.2f}s")
