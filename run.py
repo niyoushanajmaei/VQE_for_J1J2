812b832b0f48d1ba698fcdbed94ab12c7dc0385c
Praveen <pravissw@gmail.com>
2023-01-24 08:32:34 +0100

    run: No modification, COBYLA 4x3 4 layers

--- run.py
+++ run.py
@@ -76,16 +76,16 @@ def testDynamicRunner():
     seed = 50
     # ansatz in {"TwoLocal", "FeulnerHartmann"}
     ansatz = "FeulnerHartmann"
-    layers = 7
+    layers = 4
     # optimizer in {"SLSQP", "SPSA", "AMSGRAD", "COBYLA"}
-    optimizer = "SLSQP"
+    optimizer = "COBYLA"
 
     m = 3
     n = 4
     J1 = 1
     J2 = 0.5
 
-    vqe_runner = DynamicVQERunner(m, n, J1, J2, h=0, seed=seed, ansatz_rep=layers, periodic_hamiltonian=False, ansatz=ansatz, optimizer=optimizer, totalMaxIter=1000)
+    vqe_runner = DynamicVQERunner(m, n, J1, J2, h=0, seed=seed, ansatz_rep=layers, periodic_hamiltonian=False, ansatz=ansatz, optimizer=optimizer, totalMaxIter=50000)
     result = vqe_runner.run_dynamic_vqe(step_iter=np.inf, large_gradient_add=True, gradient_beta=0.1) # pass gradient_beta=None for adding one gate
     # result = vqe_runner.run_dynamic_vqe(add_layers_fresh=True)
 
